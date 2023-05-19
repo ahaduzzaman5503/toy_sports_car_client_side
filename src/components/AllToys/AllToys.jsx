2c6472bb5aecc333c60ee766297730f08e29c3d6
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-05-19 23:12:59 +0600

    allToy page data loaded

--- src/components/AllToys/AllToys.jsx
+++ src/components/AllToys/AllToys.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const AllToys = () => {
-    return (
-        <div className='container mx-auto'>
-            	All Toys Page
-        </div>
-    );
+  const toycars = useLoaderData();
+
+  return (
+    <div className="container mx-auto">
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full ">
+          {/* head */}
+          <thead   >
+            <tr >
+              <th>Seller</th>
+              <th>Toy Name</th>
+              <th>Sub Category</th>
+              <th>Price</th>
+              <th>Available Quantity</th>
+              <th>View Details</th>
+            </tr>
+          </thead>
+          <tbody className=" ">
+            {
+            toycars.map((toycar, index) => (
+              <tr key={index} >
+                <td className=" border">
+                  <div className="flex items-center space-x-3border ">
+                    <div className="">
+                      <div className="font-bold ">{toycar.seller}</div>
+                    </div>
+                  </div>
+                </td>
+                <td className="border">{toycar.toyname}</td>
+                <td className="border">{toycar.subcategory}</td>
+                <td className="border">{toycar.price}</td>
+                <td className="border">{toycar.availablequantity}</td>
+                <td className="" >
+                  <button className="btn btn-light btn-xs">View Details</button>
+                </td>
+              </tr>
+            ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllToys;
--- src/main.jsx
+++ src/main.jsx
@@ -29,7 +29,8 @@ const router = createBrowserRouter([
       },
       {
         path: "/alltoys",
-        element: <AllToys></AllToys>
+        element: <AllToys></AllToys>,
+        loader: () => fetch('http://localhost:5000/toycar')
       },
       {
         path: "/blog",
