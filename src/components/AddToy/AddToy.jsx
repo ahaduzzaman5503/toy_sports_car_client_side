103adc4031be26d70708d43269e977c1125aebb6
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-05-20 15:50:06 +0600

    add toy page working

--- src/components/AddToy/AddToy.jsx
+++ src/components/AddToy/AddToy.jsx
@@ -1,11 +1,203 @@
-import React from 'react';
+import React from "react";
 
 const AddToy = () => {
-    return (
-        <div className='container mx-auto'>
-            add some toy
+  const handleAddToy = (event) => {
+    event.preventDefault();
+
+    const sname = event.target.sname.value;
+    const toyname = event.target.toyname.value;
+    const quantity = event.target.quantity.value;
+    const price = event.target.price.value;
+    const rating = event.target.rating.value;
+    const details = event.target.details.value;
+    const email = event.target.email.value;
+    const photoUrl = event.target.photoUrl.value;
+
+    const newToy = {
+      sname, toyname, quantity, price, rating, details, email,  photoUrl,
+    };
+    console.log(newToy);
+
+    fetch("http://localhost:5000/toycardata", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newToy),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged === true) {
+          alert("Toy Car Added Successfully");
+        }
+      });
+  };
+  return (
+    <div className="container mx-auto ">
+      <form onSubmit={handleAddToy}>
+        <div className="bg-blue-400 p-3 my-3 rounded ">
+          <h1 className="text-2xl font-bold text-black text-center">
+            Add A Toy
+          </h1>
         </div>
-    );
+        <div className="grid md:grid-cols-2 gap-5">
+          {/* Sallar Name */}
+          <div className="mb-6">
+            <label
+              htmlFor="name"
+             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Saller Name
+            </label>
+            <input
+              type="text"
+              id="sname"
+              name="sname"
+             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="Sallar Name"
+              required
+            />
+          </div>
+
+          {/* Toy Name */}
+          <div className="mb-6">
+            <label
+              htmlFor="toyname"
+             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Toy Name
+            </label>
+            <input
+              type="text"
+              id="toyname"
+              name="toyname"
+             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="Your Name"
+              required
+            />
+          </div>
+
+          {/* Toy Price */}
+          <div className="mb-6">
+            <label
+              htmlFor="price"
+             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Toy Price
+            </label>
+            <input
+              type="text"
+              id="price"
+              name="price"
+             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="Price"
+              required
+            />
+          </div>
+
+          {/* Available quantity */}
+          <div className="mb-6">
+            <label
+              htmlFor="quantity"
+             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Toy Quantity
+            </label>
+            <input
+              type="text"
+              id="quantity"
+              name="quantity"
+             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="quantity"
+              required
+            />
+          </div>
+
+          {/* Rating */}
+          <div className="mb-6">
+            <label
+              htmlFor="rating"
+             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Toy Rating
+            </label>
+            <input
+              type="text"
+              id="rating"
+              name="rating"
+             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="Rating"
+              required
+            />
+          </div>
+
+          {/* Detail description */}
+          <div className="mb-6">
+            <label
+              htmlFor="details"
+             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Toy Details
+            </label>
+            <input
+              type="text"
+              id="details"
+              name="details"
+             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="Detail description"
+              required
+            />
+          </div >
+
+          {/* seller email */}
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Seller Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="name@website.com"
+              required
+            />
+          </div>
+
+          {/* Picture URL of the toy */}
+          <div className="mb-6">
+            <label
+              htmlFor="photoUrl"
+             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Toy Picture Url
+            </label>
+            <input
+              type="text"
+              id="photoUrl"
+              name="photoUrl"
+             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="Toy Photo Url"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-center">
+          <button
+            type="submit"
+           className=" w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Add Toy
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddToy;
--- src/components/AllToys/ToysDetails.jsx
+++ src/components/AllToys/ToysDetails.jsx
@@ -9,36 +9,36 @@ const ToysDetails = () => {
   return (
     <div className="container mx-auto">
         <div className="flex justify-center">
-        <div class="w-full max-w-3xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" >
+        <div className="w-full max-w-3xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" >
         <a className="" href="#">
           <img
-            class="p-8 rounded-t-lg w-3/4"
+           className="p-8 rounded-t-lg w-3/4"
             src={carSingle.picture}
             alt="product image"
           />
         </a>
-        <div class="px-5 pb-5">
+        <div className="px-5 pb-5">
           <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2">
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2">
               Toy Name: {carSingle.toyname}
             </h5>
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2">
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2">
               Saller Name: {carSingle.seller}
             </h5>
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2">
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2">
             Available Quantity: {carSingle.availablequantity}
             </h5>
-            <p class="tracking-tight text-white-700 dark:text-gray mb-2">
+            <p className="tracking-tight text-white-700 dark:text-gray mb-2">
             <span className="text-xl font-semibold">Description:- </span> {carSingle.description}
             </p>
-            <p class="tracking-tight text-gray-900 dark:text-white underline">
+            <p className="tracking-tight text-gray-900 dark:text-white underline">
             User Email: {users?.email}
             </p>
           </a>
-          <div class="flex items-center mt-2.5 mb-5">
+          <div className="flex items-center mt-2.5 mb-5">
             <svg
               aria-hidden="true"
-              class="w-5 h-5 text-yellow-300"
+             className="w-5 h-5 text-yellow-300"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -48,7 +48,7 @@ const ToysDetails = () => {
             </svg>
             <svg
               aria-hidden="true"
-              class="w-5 h-5 text-yellow-300"
+             className="w-5 h-5 text-yellow-300"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -58,7 +58,7 @@ const ToysDetails = () => {
             </svg>
             <svg
               aria-hidden="true"
-              class="w-5 h-5 text-yellow-300"
+             className="w-5 h-5 text-yellow-300"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -68,7 +68,7 @@ const ToysDetails = () => {
             </svg>
             <svg
               aria-hidden="true"
-              class="w-5 h-5 text-yellow-300"
+             className="w-5 h-5 text-yellow-300"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -78,7 +78,7 @@ const ToysDetails = () => {
             </svg>
             <svg
               aria-hidden="true"
-              class="w-5 h-5 text-yellow-300"
+             className="w-5 h-5 text-yellow-300"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -86,23 +86,23 @@ const ToysDetails = () => {
               <title>Fifth star</title>
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
             </svg>
-            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+            <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
              {carSingle.rating}
             </span>
           </div>
-          <div class="flex items-center justify-between">
-            <span class="text-3xl font-bold text-gray-900 dark:text-white">
+          <div className="flex items-center justify-between">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">
             Price:  ${carSingle.price}
             </span>
             <a
               href="#"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Buy Now
             </a>
             <a
               href="/alltoys"
-              class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+             className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Back to All Toys
             </a>
